a910a10a277e11393b08b651b864293f03b511ab
Oleg Nenashev <o.v.nenashev@gmail.com>
2017-07-31 20:38:21 +0200

    Rename the plugin to Remoting Features API

--- src/main/java/org/jenkinsci/plugins/remoting/features/RemotingFeature.java
+++ src/main/java/org/jenkinsci/plugins/remoting/features/RemotingFeature.java
@@ -21,7 +21,7 @@
  * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
  * THE SOFTWARE.
  */
-package org.jenkinsci.plugins.remoting.settings;
+package org.jenkinsci.plugins.remoting.features;
 
 import hudson.util.VersionNumber;
 import javax.annotation.CheckForNull;
--- src/main/java/org/jenkinsci/plugins/remoting/features/WorkDirSettings.java
+++ src/main/java/org/jenkinsci/plugins/remoting/features/WorkDirSettings.java
@@ -21,7 +21,7 @@
  * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
  * THE SOFTWARE.
  */
-package org.jenkinsci.plugins.remoting.settings;
+package org.jenkinsci.plugins.remoting.features;
 
 import hudson.Extension;
 import hudson.Util;
